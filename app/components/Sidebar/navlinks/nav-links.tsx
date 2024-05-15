1688565fbee0e40269345cf7a4162de1e2a55d6f
daramirezsu06 <daramirezsu06@gmail.com>
2024-05-15 01:23:08 -0500

    se migraron las requests

--- app/components/Sidebar/navlinks/nav-links.tsx
+++ app/components/Sidebar/navlinks/nav-links.tsx
@@ -24,7 +24,7 @@ export default function NavLinks() {
             href={link?.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
-            <LinkIcon className="w-6"  />
+            <LinkIcon className="w-6" />
             <p className="hidden md:block">{link?.name}</p>
           </a>
         );
--- app/components/logout/index.tsx
+++ app/components/logout/index.tsx
@@ -23,7 +23,7 @@ export default function Logout() {
       className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
     >
       <PowerIcon className="w-6" />
-      <div className="hidden md:block">Cerrar Sesión</div>
+      <div className="hidden md:block">Cerrar Sesión</div>      
     </button>
   );
 }
