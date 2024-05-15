afc7c8b1eaf3f6c3e3ca61d76cff0b8a3d32aa42
daramirezsu06 <daramirezsu06@gmail.com>
2024-05-15 13:40:37 -0500

    se hagrego el marcador a el Link activo en la dashboard

--- app/components/Sidebar/navlinks/nav-links.tsx
+++ app/components/Sidebar/navlinks/nav-links.tsx
@@ -3,8 +3,12 @@ import { useUserContext } from '../../context';
 import roles from '@/utils/arrayMenu/roles';
 import { useEffect, useState } from 'react';
 import { RoleItem } from '@/utils/types/rolesNavLinkInterface';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import Link from 'next/link';
 
 export default function NavLinks() {
+  const pathname = usePathname();
   const [arrayNavLinks, setArrayNavLinks] = useState<RoleItem[]>([]);
   const { user } = useUserContext();
 
@@ -19,14 +23,19 @@ export default function NavLinks() {
       {arrayNavLinks.map((link) => {
         const LinkIcon = link?.icon;
         return (
-          <a
+          <Link
             key={link?.name}
             href={link?.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link?.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
