35d602604a75b4533b28e6631dbb0824b8741a29
daramirezsu06 <daramirezsu06@gmail.com>
2024-05-15 14:49:36 -0500

    paso de navLink a server component

--- app/components/Sidebar/navlinks/nav-links.tsx
+++ app/components/Sidebar/navlinks/nav-links.tsx
@@ -1,22 +1,16 @@
-'use client';
 import { useUserContext } from '../../context';
 import roles from '@/utils/arrayMenu/roles';
 import { useEffect, useState } from 'react';
 import { RoleItem } from '@/utils/types/rolesNavLinkInterface';
-import { usePathname } from 'next/navigation';
+// import { usePathname } from 'next/navigation';
+import { cookies } from 'next/headers';
 import clsx from 'clsx';
 import Link from 'next/link';
 
-export default function NavLinks() {
-  const pathname = usePathname();
-  const [arrayNavLinks, setArrayNavLinks] = useState<RoleItem[]>([]);
-  const { user } = useUserContext();
+export default async function NavLinks() {
+  const user = JSON.parse(cookies().get('user')?.value);
 
-  useEffect(() => {
-    if (user?.role) {
-      setArrayNavLinks(roles[user.role]);
-    }
-  }, [user]);
+  const arrayNavLinks = roles[user?.role];
 
   return (
     <>
@@ -26,12 +20,7 @@ export default function NavLinks() {
           <Link
             key={link?.name}
             href={link?.href}
-            className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-              {
-                'bg-sky-100 text-blue-600': pathname === link.href,
-              },
-            )}
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link?.name}</p>
