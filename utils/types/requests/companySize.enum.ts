50bf96df4384651b2c671204d744ce850b28763c
SebasPCDev <sebpa.16@gmail.com>
2024-05-17 22:47:22 -0500

    modificacion formulario de compañia y valores de tamaño de empresa

--- utils/types/requests/companySize.enum.ts
+++ utils/types/requests/companySize.enum.ts
@@ -1,13 +1,18 @@
 export enum CompanySize {
-  MICRO_BUSINESS = '0 - 30',
-  SMALL_BUSINESS = '31 - 100',
-  MEDIUM_BUSINESS = '101 - 500',
-  LARGE_BUSINESS = '501 - en adelante'
+  MICRO_BUSINESS = '1 - 10',
+  SMALL_BUSINESS = '10 - 50',
+  MEDIUM_BUSINESS = '50 - 250',
+  LARGE_BUSINESS = '250 - 500',
+  EXTRALARGE_BUSINESS = 'más de 500',
 }
 
 export const COMPANY_SIZE = [
   { id: CompanySize.MICRO_BUSINESS, value: CompanySize.MICRO_BUSINESS },
   { id: CompanySize.SMALL_BUSINESS, value: CompanySize.SMALL_BUSINESS },
   { id: CompanySize.MEDIUM_BUSINESS, value: CompanySize.MEDIUM_BUSINESS },
-  { id: CompanySize.LARGE_BUSINESS, value: CompanySize.LARGE_BUSINESS }
-]
+  { id: CompanySize.LARGE_BUSINESS, value: CompanySize.LARGE_BUSINESS },
+  {
+    id: CompanySize.EXTRALARGE_BUSINESS,
+    value: CompanySize.EXTRALARGE_BUSINESS,
+  },
+];
