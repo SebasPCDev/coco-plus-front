0ce6e0efd3d7b9ff88a96530b2e9784454025b7e
daramirezsu06 <daramirezsu06@gmail.com>
2024-05-26 18:31:00 -0500

    commit previo oull

--- app/components/coworkings2/coworking/index.tsx
+++ app/components/coworkings2/coworking/index.tsx
@@ -1,47 +1,56 @@
-import IResponseCoworking from '@/utils/types/coworkingsResponse'
-import Image from 'next/image'
-import Link from 'next/link'
-import TimeIcon from '../../icons/TimeIcon'
-import PositionIcon from '../../icons/Position'
+import IResponseCoworking from '@/utils/types/coworkingsResponse';
+import Image from 'next/image';
+import Link from 'next/link';
+import TimeIcon from '../../icons/TimeIcon';
+import PositionIcon from '../../icons/Position';
 
 const Coworking = ({ coworking }: { coworking: IResponseCoworking }) => {
-
-  const url = process.env.NEXT_PUBLIC_FRONT_URL
   return (
-    <article className='relative flex flex-col justify-between shadow-lg w-[300px] bg-[#f4faf7] text-sm'>
-      <Link className='relative group overflow-hidden rounded-t' href={`${url}/coworkings/${coworking.id}`}>
-        <Image width={300} height={250} className='block h-auto m-auto rounded-t shadow-md group-hover:scale-110 transition duration-500 object-cover' src={coworking.thumbnail || ''} alt={coworking.name || ''} />
-        <div className="absolute bg-black w-full h-full opacity-0 transition-opacity duration-500 group-hover:opacity-10" />
+    <article className="relative flex w-[300px] flex-col justify-between bg-[#f4faf7] text-sm shadow-lg">
+      <Link
+        className="group relative overflow-hidden rounded-t"
+        href={`/coworkings/${coworking.id}`}
+      >
+        <Image
+          width={300}
+          height={250}
+          className="m-auto block h-auto rounded-t object-cover shadow-md transition duration-500 group-hover:scale-110"
+          src={coworking.thumbnail || ''}
+          alt={coworking.name || ''}
+        />
+        <div className="absolute h-full w-full bg-black opacity-0 transition-opacity duration-500 group-hover:opacity-10" />
       </Link>
-      <main className='px-4'>
-        <Link href={`${url}/coworkings/${coworking.id}`}>
-          <h3 className="font-semibold text-base my-4">{coworking.name}</h3>
+      <main className="px-4">
+        <Link href={`/coworkings/${coworking.id}`}>
+          <h3 className="my-4 text-base font-semibold">{coworking.name}</h3>
         </Link>
-        <hr className='pt-4' />
-        <div className='flex items-center gap-4'>
+        <hr className="pt-4" />
+        <div className="flex items-center gap-4">
           <TimeIcon className="w-6 text-gray-600" />
-          <p className=''>Horario</p>
+          <p className="">Horario</p>
         </div>
-        <span className='my-2 ml-10 leading-6'>
+        <span className="my-2 ml-10 leading-6">
           {coworking.open?.slice(0, 5)} am - {coworking.close?.slice(0, 5)} pm
         </span>
-        <hr className='pt-4' />
-        <div className='flex items-center gap-4'>
+        <hr className="pt-4" />
+        <div className="flex items-center gap-4">
           <PositionIcon className="w-6 text-gray-600" />
-          <p className=''>Ubicación</p>
+          <p className="">Ubicación</p>
         </div>
-        <div className='my-2 ml-10 leading-6'>
-          <p>{coworking.country} - {coworking.state}</p>
+        <div className="my-2 ml-10 leading-6">
+          <p>
+            {coworking.country} - {coworking.state}
+          </p>
           <p>{coworking.city}</p>
           <p>{coworking.address}</p>
         </div>
       </main>
       <footer>
-        <Link href={`${url}/coworkings/${coworking.id}`}>
-          <p className='text-sm text-right p-4'>Ver detalle</p>
+        <Link href={`/coworkings/${coworking.id}`}>
+          <p className="p-4 text-right text-sm">Ver detalle</p>
         </Link>
       </footer>
     </article>
-  )
-}
-export default Coworking
+  );
+};
+export default Coworking;
